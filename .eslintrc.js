172bffbbf48bee66c4fc8b95802d6ca2fe525a80
David Holovii <mirage.present@gmail.com>
2019-01-06 13:47:10 +0200

    Changed elsint template

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,28 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    ecmaVersion: '2017',
     sourceType: 'module'
   },
   env: {
     browser: true,
+    es6: true,
+    node: true,
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
+  extends: [
+    "airbnb-base",
+    "plugin:vue/recommended"
+  ],
   // required to lint *.vue files
   plugins: [
-    'html'
+    'vue',
+    'html',
   ],
   // add your custom rules here
-  'rules': {
+  rules: {
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
--- src/main.js
+++ src/main.js
@@ -1,30 +1,31 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import VueApollo from 'vue-apollo'
-import App from './App'
-import router from './router'
 
-import ApolloCLient from 'apollo-client'
-import HttpLink from 'apollo-link-http'
-import InMemoryCache from 'apollo-cache-inmemory'
+import Vue from 'vue';
+import VueApollo from 'vue-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-Vue.config.productionTip = false
+import router from './router';
+import App from './App.vue';
+
+Vue.config.productionTip = false;
 
 const link = new HttpLink({
-  uri: 'https://playing-with-lumen.test/graphql'
-})
+  uri: 'https://playing-with-lumen.test/graphql',
+});
 
-const apolloClient = new ApolloCLient({
+const apolloClient = new ApolloClient({
   link,
-  cache: new InMemoryCache()
-})
+  cache: new InMemoryCache(),
+});
 
 const apolloProvider = new VueApollo({
-  defaultClient: apolloClient
-})
+  defaultClient: apolloClient,
+});
 
-Vue.use(VueApollo)
+Vue.use(VueApollo);
 
 /* eslint-disable no-new */
 new Vue({
@@ -32,5 +33,5 @@ new Vue({
   router,
   template: '<App/>',
   components: { App },
-  apolloProvider
-})
+  apolloProvider,
+});
